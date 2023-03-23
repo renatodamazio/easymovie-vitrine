44910538a6cd94fd13eab28943442a324662df38
Renato Damázio <renatopdamazio@gmail.com>
2023-03-22 21:55:24 -0300

    Navigation with replace tag url and video pause

--- site.config.js
+++ site.config.js
@@ -12,24 +12,80 @@
  */
 
 const makeId = (length) => {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-};
+  var result = ''
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var charactersLength = characters.length
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
 
 const DATA = {
-    "base": {
-        "static": "static/",
-        "revid": makeId(8),
-        "title": "EasyMovie | Plataforma de contratação e produção de vídeo",
-        "metaDesc": "Receba propostas em poucas horas para a criação do seu vídeo e contrate o produtor ideal. Encontre produtoras e profissionais de produção de vídeo."
-    }
-};
+  base: {
+    static: 'static/',
+    revid: makeId(8),
+    title: 'EasyMovie | Plataforma de contratação e produção de vídeo',
+    metaDesc:
+      'Receba propostas em poucas horas para a criação do seu vídeo e contrate o produtor ideal. Encontre produtoras e profissionais de produção de vídeo.',
+  },
+
+  items: [
+    {
+      title: 'John Wick Chapter 4',
+      category: 'animacao',
+      url: 'john-wick',
+      hash_tags: ['#editor', '#animação', '#livro', '#ilustração'],
+      skills: ['Edição', 'Capitação', 'Iluminação'],
+      tags: ['Institucional', 'livraria'],
+      description: 'Lorem Ipsum',
+      videoId: "https://customer-4tl6n57yx29lfuzn.cloudflarestream.com/1661365099dc9a0712dcb988762bbcf0/iframe?poster=https%3A%2F%2Fcustomer-4tl6n57yx29lfuzn.cloudflarestream.com%2F1661365099dc9a0712dcb988762bbcf0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D5s%26height%3D600",
+      cover:
+        'https://cdn.bhdw.net/im/john-wick-chapter-3-wallpaper-30271_w635.webp',
+    },
+    {
+      title: 'Avengers',
+      category: 'publicidade',
+      url: 'avengers',
+      hash_tags: ['#editor', '#animação', '#livro', '#ilustração'],
+      skills: ['Edição', 'Capitação', 'Iluminação'],
+      tags: ['Institucional', 'livraria'],
+      videoId: "https://customer-4tl6n57yx29lfuzn.cloudflarestream.com/1661365099dc9a0712dcb988762bbcf0/iframe?poster=https%3A%2F%2Fcustomer-4tl6n57yx29lfuzn.cloudflarestream.com%2F1661365099dc9a0712dcb988762bbcf0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D5s%26height%3D600",
+      description:
+        'Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias.',
+      cover:
+        'https://i.pinimg.com/originals/62/5f/6b/625f6beceff0ba88f5787c20f1a202c6.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      category: 'edicao',
+      url: 'game-of-thrones',
+      hash_tags: ['#editor', '#animação', '#livro', '#ilustração'],
+      skills: ['Edição', 'Capitação', 'Iluminação'],
+      tags: ['Institucional', 'livraria'],
+      videoId: "https://customer-4tl6n57yx29lfuzn.cloudflarestream.com/1661365099dc9a0712dcb988762bbcf0/iframe?poster=https%3A%2F%2Fcustomer-4tl6n57yx29lfuzn.cloudflarestream.com%2F1661365099dc9a0712dcb988762bbcf0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D5s%26height%3D600",
+      description:
+        'Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias. Graduanda em Comunicação Social - Publicidade e Propaganda pela Pontifícia Universidade Católica de Minas Gerais (PUC Minas). Técnica em Administração pelo Instituto Federal de Minas Gerais (IFMG).Atuou como estagiária em comunicação das academias UAU Fit, criando conteúdos para web e redes sociais, conteúdos off-line e gerenciado mídias.',
+      cover:
+        'https://www.igeeksblog.com/wp-content/uploads/2021/03/Game-of-Thrones-Season-8-iPhone-Wallpaper.jpg',
+    },
+    {
+      title: 'True Detective season 1',
+      category: 'game-play',
+      url: 'true-detective',
+      hash_tags: ['#editor', '#animação', '#livro', '#ilustração'],
+      skills: ['Edição', 'Capitação', 'Iluminação'],
+      tags: ['Institucional', 'livraria'],
+      description: 'Lorem Ipsum',
+      videoId: "https://customer-4tl6n57yx29lfuzn.cloudflarestream.com/1661365099dc9a0712dcb988762bbcf0/iframe?poster=https%3A%2F%2Fcustomer-4tl6n57yx29lfuzn.cloudflarestream.com%2F1661365099dc9a0712dcb988762bbcf0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D5s%26height%3D600",
+
+      cover: 'https://images7.alphacoders.com/645/645727.jpg',
+    },
+  ],
+}
+
 
 module.exports = {
-    data: DATA
-};
+  data: DATA,
+}
--- src/assets/js/modules/pageHome.js
+++ src/assets/js/modules/pageHome.js
@@ -21,30 +21,101 @@ const filterVideos = () => {
 }
 
 const carousel = () => {
+  const counter = (event) => {
+    const videoId = $(
+      `.modal-portfolio__video__wrapper:eq(${event.item.index})`
+    ).data('video-url')
+    showSideBarSection(videoId)
+
+    const url = window.location.href.split('/')
+    const deleteId = url.slice(0, -1)
+
+    const newUrl = deleteId.join('/') + '/' + videoId
+    changeURL(newUrl)
+    handleVideoPlayer(event.item.index)
+  }
+
   const carousel_video = $(
     '.modal-portfolio__carousel__owl-slider'
   ).owlCarousel({
     items: 1,
-    loop: true,
+    loop: false,
     dots: false,
+    onTranslate: counter,
   })
 
-  $('.slide-control.arrow-left').click(() =>
+  $('.slide-control.arrow-left').click(() => {
     carousel_video.trigger('prev.owl.carousel')
-  )
+  })
   $('.slide-control.arrow-right').click(() =>
     carousel_video.trigger('next.owl.carousel')
   )
 }
 
-const handleURL = () => {
-  const url = new URL(window.location)
+const handleVideoPlayer = () => {
+  $('.data-video-id').each(function () {
+    const currentVideoId = $(this).data('video-id')
+    $(this).html(`
+    <iframe
+    src="${currentVideoId}"
+    style="
+      border: none;
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+    "
+    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+    allowfullscreen="true"
+  ></iframe>
+    `)
+  });
+}
 
+const getVideoIndex = (url) => {
+  let indexVideo = 0
+  $('.modal-portfolio__video__wrapper').each((index, item) => {
+    if ($(item).data('video-url') === url) {
+      indexVideo = index
+    }
+  })
+  return indexVideo
+}
+
+const showSideBarSection = (videoId) => {
+  $('.modal-portfolio__side-bar__wrapper')
+    .stop(true, true)
+    .fadeOut('fast', () => {
+      $(`[data-side-bar-id="${videoId}"]`)
+        .delay(900)
+        .stop(true, true)
+        .fadeIn('slow')
+    })
+}
+
+const changeURL = (url) => {
+  window.history.pushState('Object', 'Categoria JavaScript', url)
+}
+
+const handleURL = () => {
   $('.__handle-url').click(function (e) {
     e.preventDefault()
     const url = $(this).attr('href')
+    const videoURL = url.split('/')
+    const videoId = videoURL[videoURL.length - 1]
+    const videoIndex = getVideoIndex(videoId)
 
-    window.history.pushState('Object', 'Categoria JavaScript', url)
+    $('.modal-portfolio__carousel__owl-slider').trigger(
+      'to.owl.carousel',
+      videoIndex
+    )
+
+    showSideBarSection(videoId)
+
+    changeURL(url)
+
+    handleVideoPlayer(videoIndex)
     return
   })
 }
@@ -106,9 +177,11 @@ const Home = () => {
     })
 
     $('.modal-portfolio__close').click(() => {
-      $('.modal-portfolio').stop().fadeOut()
-      window.history.pushState('', '', "/")
+      $('.modal-portfolio').stop().fadeOut();
+      handleVideoPlayer();
+      window.history.pushState('', '', '/')
     })
+
     filterCategoriesHome()
     filterVideos()
     readMoreLess()
